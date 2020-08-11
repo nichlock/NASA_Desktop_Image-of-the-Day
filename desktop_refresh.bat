130f8cd89fb37542af6fcb3410e92e30693d830e
Nicholas Steele <nichlock@gmail.com>
2020-08-11 09:56:33 -0400

    Update desktop_refresh.bat

--- desktop_refresh.bat
+++ desktop_refresh.bat
@@ -1,6 +1,6 @@
 REM You don't need to use this file, but, if you want the JAR file seperate from your images, it can be useful.
 REM The runnable jar file
-JAR_LOCATION="[JAR location]"
+JAR_LOCATION=[JAR location]
 REM The directory to store the images in. Don't use quotes here.
-IMAGE_LOCATION="[where to save the image]"
+IMAGE_LOCATION=[where to save the image]
 %JAR_LOCATION% %IMAGE_LOCATION%
