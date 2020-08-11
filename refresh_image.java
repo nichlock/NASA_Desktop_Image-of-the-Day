ad74dd1f471d572cc985acf7066d7ff7fbd8085e
Nicholas Steele <nichlock@gmail.com>
2020-08-11 09:40:45 -0400

    Update refresh_image.java
    + Added argument to change the directory of the images
    + Now  interprets the image type to avoid coloring issues when accessing a PNG
    + Improved Exception handling
    - Removed an unused import

--- refresh_image.java
+++ refresh_image.java
@@ -1,71 +1,120 @@
-//This Java file gets the location of the NASA image of the day and downloads it to a new file either labeled img.jpg or img2.jpg
+
+/**
+ * This Java file gets the location of the NASA image of the day and downloads it to a new file either labeled img.jpg or img2.jpg
+ */
+
+// Getting the image and processing it
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.net.URL;
-import java.net.URLConnection;
+import javax.imageio.ImageIO;
 
+// Reading XML documents
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
-
-import javax.imageio.ImageIO;
-import javax.swing.JOptionPane;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
 
-public class Main {
+// Printing  errors without a console
+import javax.swing.JOptionPane;
 
-    public static void main(String args[]) { // This is the first section of code to run
-    	saveImage(parseXml()); // Set the image to the image pulled from NASA's RSS feed
-    }
-    
-    private static String parseXml() // The second class to run, returns the image link pulled from the RSS feed
-    {								 // This class was 
-    	String imagelink = "";
-        try 
-        {
-            URLConnection conn = new URL("https://www.nasa.gov/rss/dyn/lg_image_of_the_day.rss").openConnection();
-            DocumentBuilderFactory dbFactory=DocumentBuilderFactory.newInstance();
-            DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
-            Document doc=dBuilder.parse(conn.getInputStream());
-            doc.getDocumentElement().normalize();
-            NodeList nList=doc.getElementsByTagName("item");
-            Node nNode=nList.item(0);
-          
+public class refresh_image {
 
-                    if(nNode.getNodeType()==Node.ELEMENT_NODE)
-                    {
-                        Element eElement=(Element) nNode;
-                        String temp=eElement.getElementsByTagName("enclosure").item(0).getAttributes().getNamedItem("url")+"";
-                        imagelink=temp.replace("url=\"", "").replace("\"", "").trim();
-                    }
-                    return imagelink;
-            
-            
-        } catch (Exception ex) { //If the RSS can't be accessed then open a window to warn the user
-            JOptionPane.showMessageDialog(null, "Error while getting RSS.\nPerhaps try checking your internet connection?");
-            return null;
-        }
-        
-    }
-    public static void saveImage(String imageURL) // The third class to run, this saves the image to the specified location
-    {
-    	// For troubleshooting:
-        System.out.println(imageURL.substring(0,4) + "s" + imageURL.substring(4));
-        try { //try the following code:
-        	
-            // Get the image from the URL, and set it to the image files
-            URL url = new URL(imageURL.substring(0,4) + "s" + imageURL.substring(4));
-            BufferedImage imageS = null;
-            imageS = ImageIO.read(url);
-            ImageIO.write(imageS, "jpg", new File("img.jpg")); //writes to two image files, this allows for Windows 7 compatibility
-            ImageIO.write(imageS, "jpg", new File("img2.jpg"));
-            
-        } catch (Exception ex) { // If the previous code failed for some reason, then open a windows that states the failure
-            JOptionPane.showMessageDialog(null, "Error while writing to the image.");
-            ex.printStackTrace();
-        }
-    }
+	static String nasa_iotd_rss = "https://www.nasa.gov/rss/dyn/lg_image_of_the_day.rss";
+
+	/**
+	 * Saves images grabbed from RSS feed.
+	 * 
+	 * @param First argument is the location to save the images. If no argument is
+	 *              supplied, it saves to local directory
+	 */
+	public static void main(String args[]) {
+		// Set the image to the one pulled from NASA's RSS feed
+		if (args.length == 1)
+			saveImage(parseXml(), args[0]); // Use given directory
+		else
+			saveImage(parseXml(), ""); // Use local directory
+	}
+
+	/**
+	 * Get the image location form the XML format RSS feed
+	 * 
+	 * @return String format URL for the image to download
+	 */
+	private static String[] parseXml() {
+		try {
+			// Get and prepare the XML document
+			DocumentBuilder dBuilder = (DocumentBuilderFactory.newInstance()).newDocumentBuilder();
+			Document doc = dBuilder.parse((new URL(nasa_iotd_rss).openConnection()).getInputStream());
+			doc.getDocumentElement().normalize();
+			NodeList nList = doc.getElementsByTagName("item");
+			Node nNode = nList.item(0);
+
+			// Get the link to the image and return it
+			if (nNode.getNodeType() == Node.ELEMENT_NODE) {
+				Element eElement = (Element) nNode;
+				// Get the image URL
+				String image_url = eElement.getElementsByTagName("enclosure").item(0).getAttributes()
+						.getNamedItem("url") + "";
+				// Get the image format, which is in written in a more standard way than just
+				// the end of URL
+				String format = eElement.getElementsByTagName("enclosure").item(0).getAttributes().getNamedItem("type")
+						+ "";
+				// Trim to get only the values
+				image_url = image_url.replace("url=\"", "").replace("\"", "").trim();
+				format = format.replace("type=", "").replace("image/", "").replace("\"", "").trim();
+				// Pack into an array for saveImage
+				String[] str_arr = { image_url, format };
+				return str_arr;
+			}
+//			return imagelink;
+
+		} catch (Exception ex) { // If the RSS can't be accessed then open a window to warn the user
+			JOptionPane.showMessageDialog(null,
+					"Error while getting RSS.\nPerhaps try checking your internet connection?");
+			System.exit(0);
+		}
+		return null; // Program should never get here, but this is needed for Java to compile
+
+	}
+
+	/**
+	 * This saves the image to the specified location
+	 * 
+	 * @param imageURL  The URL to the image
+	 * @param directory Where to save images
+	 */
+	public static void saveImage(String image[], String directory) {
+		// Get the URL and type first
+		String image_url = image[0];
+		String image_format = image[1];
+		// Get the image from the URL, and set it to the image files
+		try {
+			// Convert from http to https and get the URL object
+			URL url = new URL(image_url.substring(0, 4) + "s" + image_url.substring(4));
+
+			// Print some troubleshooting info
+			System.out.println("Image URL: " + url.toString());
+			System.out.println("Saving image from URL to " + directory + "img." + image_format);
+			// Get the image
+			BufferedImage iotd = null;
+			iotd = ImageIO.read(url);
+			// Write to the two image files.
+			// Using two image files for Windows 7 compatibility
+			ImageIO.write(iotd, image_format, new File(directory + "img." + image_format));
+			ImageIO.write(iotd, image_format, new File(directory + "img2." + image_format));
+
+		} catch (FileNotFoundException e) { // Could not write to file
+			JOptionPane.showMessageDialog(null,
+					"Error while writing to the image.\nCould not write to image at location: " + directory + "img.*");
+			e.printStackTrace();
+		} catch (Exception e) { // If getting the image failed, show why
+			JOptionPane.showMessageDialog(null, "Error while writing to the image.");
+			e.printStackTrace();
+		}
+	}
 
 }
